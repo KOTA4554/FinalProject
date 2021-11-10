be10e6d7d6097e123b5bb578e1e8749978542311
jminsu2 <87969361+jminsu2@users.noreply.github.com>
2021-11-10 15:42:05 +0900

    민수커밋

---
+++ 01_SpringDI/src/main/java/com/kh/spring/di/controller/DependencyAnno.java
@@ -0,0 +1,48 @@
+package com.kh.spring.di.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.kh.spring.di.anno.vo.Person;
+
+/**
+ * Servlet implementation class DependencyAnno
+ */
+public class DependencyAnno extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DependencyAnno() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ApplicationContext ctx = new GenericXmlApplicationContext("/anno-di-context.xml");
+		
+		Person p = (Person)ctx.getBean("person");
+		
+		System.out.println(p);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ 01_SpringDI/src/main/java/com/kh/spring/di/model/vo/TvMgr.java
@@ -0,0 +1,16 @@
+package com.kh.spring.di.model.vo;
+
+public class TvMgr {
+	private TV tv;
+	
+	public TvMgr() { }
+
+	public TV getTv() {
+		return tv;
+	}
+
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+
+}
