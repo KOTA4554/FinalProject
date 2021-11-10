a0def382d33fb86475f25e88e8447d0628174eb1
dragonJ <yj9678joo@gmail.com>
2021-11-10 15:41:07 +0900

    커밋완료

---
+++ 01_SpringDI/src/main/java/com/kh/spring/di/model/vo/LgTV.java
@@ -0,0 +1,45 @@
+package com.kh.spring.di.model.vo;
+
+public class LgTV implements TV{
+
+	@Override
+	public void turnOn() {
+		System.out.println("전원을 켭니다!");
+		
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("전원을 종료합니다.");
+		
+	}
+
+	@Override
+	public void volumeUp() {
+		System.out.println("소리를 높입니다.");
+		
+	}
+
+	@Override
+	public void volumeDown() {
+		System.out.println("소리를 낮춥니다.");
+		
+	}
+	/*
+	public void turnOn() {
+		System.out.println("전원을 켭니다!");
+	}
+	
+	public void turnOff() {
+		System.out.println("전원을 종료합니다.");
+	}
+	
+	public void soundUp() {
+		System.out.println("소리를 높입니다.");
+	}
+	
+	public void soundDown() {
+		System.out.println("소리를 낮춥니다.");
+	}
+	*/
+}
---
+++ 01_SpringDI/src/main/java/com/kh/spring/di/xml/vo/Job.java
@@ -0,0 +1,7 @@
+package com.kh.spring.di.xml.vo;
+
+public interface Job {
+	// 직업 클래스
+	void work(String place);
+	
+}
